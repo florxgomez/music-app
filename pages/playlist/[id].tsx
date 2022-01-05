a0f0a2ea9bb2b246701b83d9dfc0887d4fd1dcf3
Florencia Gomez <florxgomez@gmail.com>
2022-01-05 20:47:36 -0300

    fix auth

--- pages/playlist/[id].tsx
+++ pages/playlist/[id].tsx
@@ -25,11 +25,20 @@ const Playlist = ({ playlist }) => {
 };
 
 export const getServerSideProps = async ({ query, req }) => {
-  const { id } = validateToken(req.cookies.TRAX_ACCESS_TOKEN);
+  let user;
+  try {
+    user = validateToken(req.cookies.TRAX_ACCESS_TOKEN);
+  } catch (e) {
+    return {
+      permanent: false,
+      destination: "/signin",
+    };
+  }
+
   const [playlist] = await prisma.playlist.findMany({
     where: {
       id: +query.id,
-      userId: id,
+      userId: user.id,
     },
     include: {
       songs: {
