d19685bde189d93694815b323435eb47cdd03e19
Florencia Gomez <florxgomez@gmail.com>
2022-01-06 00:54:53 -0300

    player controls

--- components/player.tsx
+++ components/player.tsx
@@ -21,11 +21,104 @@ import {
   MdOutlineRepeat,
 } from "react-icons/md";
 import { useStoreActions } from "easy-peasy";
+import { formatTime } from "../lib/formatters";
 
-const Player = () => {
+const Player = ({ songs, activeSong }) => {
+  const [playing, setPlaying] = useState(false);
+  const [index, setIndex] = useState(
+    songs.findIndex((song) => song.id === activeSong.id)
+  );
+  const [seek, setSeek] = useState(0.0);
+  const [isSeeking, setIsSeeking] = useState(false);
+  const [repeat, setRepeat] = useState(false);
+  const [shuffle, setShuffle] = useState(false);
+  const [duration, setDuration] = useState(0.0);
+  const soundRef = useRef(null);
+  const repeatRef = useRef(repeat);
+  const setActiveSong = useStoreActions((state: any) => state.changeActiveSong);
+
+  useEffect(() => {
+    let timerId;
+    if (playing && !isSeeking) {
+      const f = () => {
+        setSeek(soundRef.current.seek());
+        timerId = requestAnimationFrame(f);
+      };
+
+      timerId = requestAnimationFrame(f);
+      return () => cancelAnimationFrame(timerId);
+    }
+    cancelAnimationFrame(timerId);
+  }, [playing, isSeeking]);
+
+  useEffect(() => {
+    setActiveSong(songs[index]);
+  }, [index, setActiveSong, songs]);
+
+  useEffect(() => {
+    repeatRef.current = repeat;
+  }, [repeat]);
+
+  const setPlayState = (value) => {
+    setPlaying(value);
+  };
+
+  const onShuffle = () => {
+    setShuffle((state) => !state);
+  };
+
+  const onRepeat = () => {
+    setRepeat((state) => !state);
+  };
+  const prevSong = () => {
+    setIndex((state) => {
+      return state ? state - 1 : songs.length - 1;
+    });
+  };
+
+  const nextSong = () => {
+    setIndex((state) => {
+      if (shuffle) {
+        const next = Math.floor(Math.random() * songs.length);
+        if (next === state) {
+          return nextSong();
+        }
+        return next;
+      }
+      return state === songs.length - 1 ? 0 : state + 1;
+    });
+  };
+
+  const onEnd = () => {
+    if (repeatRef.current) {
+      setSeek(0);
+      soundRef.current.seek(0);
+    } else {
+      nextSong();
+    }
+  };
+
+  const onLoad = () => {
+    const songDuration = soundRef.current.duration();
+    setDuration(songDuration);
+  };
+
+  const onSeek = (e) => {
+    setSeek(parseFloat(e[0]));
+    soundRef.current.seek(e[0]);
+  };
+
   return (
     <Box>
-      <Box>{/* <ReactHowler /> */}</Box>
+      <Box>
+        <ReactHowler
+          playing={playing}
+          src={activeSong.url}
+          ref={soundRef}
+          onLoad={onLoad}
+          onEnd={onEnd}
+        />
+      </Box>
       <Center color="gray.600">
         <ButtonGroup>
           <IconButton
@@ -34,6 +127,8 @@ const Player = () => {
             aria-label="shuffle"
             fontSize="24px"
             icon={<MdShuffle />}
+            color={shuffle ? "white" : "gray.600"}
+            onClick={onShuffle}
           />
         </ButtonGroup>
         <ButtonGroup>
@@ -43,17 +138,10 @@ const Player = () => {
             aria-label="skip"
             fontSize="24px"
             icon={<MdSkipPrevious />}
+            onClick={prevSong}
           />
           <ButtonGroup>
-            <IconButton
-              outline="none"
-              variant="link"
-              aria-label="play"
-              fontSize="40px"
-              icon={<MdOutlinePlayCircleFilled />}
-              color="white"
-            />
-            <ButtonGroup>
+            {playing ? (
               <IconButton
                 outline="none"
                 variant="link"
@@ -61,24 +149,38 @@ const Player = () => {
                 fontSize="40px"
                 color="white"
                 icon={<MdOutlinePauseCircleFilled />}
+                onClick={() => setPlayState(false)}
+              />
+            ) : (
+              <IconButton
+                outline="none"
+                variant="link"
+                aria-label="play"
+                fontSize="40px"
+                icon={<MdOutlinePlayCircleFilled />}
+                color="white"
+                onClick={() => setPlayState(true)}
+              />
+            )}
+            <ButtonGroup>
+              <IconButton
+                outline="none"
+                variant="link"
+                aria-label="next"
+                fontSize="24px"
+                icon={<MdSkipNext />}
+                onClick={nextSong}
               />
               <ButtonGroup>
                 <IconButton
                   outline="none"
                   variant="link"
-                  aria-label="next"
+                  aria-label="repeat"
                   fontSize="24px"
-                  icon={<MdSkipNext />}
+                  icon={<MdOutlineRepeat />}
+                  onClick={onRepeat}
+                  color={repeat ? "white" : "gray.600"}
                 />
-                <ButtonGroup>
-                  <IconButton
-                    outline="none"
-                    variant="link"
-                    aria-label="shuffle"
-                    fontSize="24px"
-                    icon={<MdOutlineRepeat />}
-                  />
-                </ButtonGroup>
               </ButtonGroup>
             </ButtonGroup>
           </ButtonGroup>
@@ -87,15 +189,19 @@ const Player = () => {
       <Box color="gray.600">
         <Flex justify="center" align="center">
           <Box width="10%">
-            <Text fontSize="x-small">1:21</Text>
+            <Text fontSize="x-small">{formatTime(seek)}</Text>
           </Box>
           <Box width="80%">
             <RangeSlider
               aria-label={["min, max"]}
               step={0.1}
               min={0}
-              max={321}
               id="player-range"
+              max={duration ? duration.toFixed(2) : 0}
+              onChange={onSeek}
+              value={[seek]}
+              onChangeStart={() => setIsSeeking(true)}
+              onChangeEnd={() => setIsSeeking(false)}
             >
               <RangeSliderTrack bg="gray.800">
                 <RangeSliderFilledTrack bg="gray.600" />
@@ -104,7 +210,7 @@ const Player = () => {
             </RangeSlider>
           </Box>
           <Box width="10%" textAlign="right">
-            <Text fontSize="x-small">321</Text>
+            <Text fontSize="x-small">{formatTime(duration)}</Text>
           </Box>
         </Flex>
       </Box>
--- components/songsTable.tsx
+++ components/songsTable.tsx
@@ -3,8 +3,17 @@ import { Table, Thead, Td, Tr, Tbody, Th, IconButton } from "@chakra-ui/react";
 import { BsFillPlayFill } from "react-icons/bs";
 import { AiOutlineClockCircle } from "react-icons/ai";
 import { formatDate, formatTime } from "../lib/formatters";
+import { useStoreActions } from "easy-peasy";
 
 const SongsTable = ({ songs }) => {
+  const playSongs = useStoreActions((store: any) => store.changeActiveSongs);
+  const setActiveSong = useStoreActions((store: any) => store.changeActiveSong);
+
+  const handlePlay = (activeSong?) => {
+    setActiveSong(activeSong || songs[0]);
+    playSongs(songs);
+  };
+
   return (
     <Box bg="transparent" color="white">
       <Box padding="10px" marginBottom="20px">
@@ -15,6 +24,7 @@ const SongsTable = ({ songs }) => {
             size="lg"
             isRound
             aria-label="play"
+            onClick={() => handlePlay()}
           />
         </Box>
         <Table variant="unstyled">
@@ -22,6 +32,7 @@ const SongsTable = ({ songs }) => {
             <Tr>
               <Th>#</Th>
               <Th>Title</Th>
+              <Th>Artist</Th>
               <Th>Date Added</Th>
               <Th>
                 <AiOutlineClockCircle />
@@ -39,9 +50,11 @@ const SongsTable = ({ songs }) => {
                 }}
                 key={song.id}
                 cursor="pointer"
+                onClick={() => handlePlay(song)}
               >
                 <Td>{i + 1}</Td>
                 <Td>{song.name}</Td>
+                <Td>{song.artist.name}</Td>
                 <Td>{formatDate(song.createdAt)}</Td>
                 <Td>{formatTime(song.duration)}</Td>
               </Tr>
